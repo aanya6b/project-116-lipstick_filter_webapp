888f6c2aae97d0e3a9dc140ef2ae579be9c8b5f5
aanya6b <109451124+aanya6b@users.noreply.github.com>
2022-07-17 10:51:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX = 0;
+noseY = 0;
+
+function preload()
+{
+    lipstick = loadImage("https://i.postimg.cc/yxnkLRWx/l1.png");
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide()
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("Pose net is Initialised.");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y+18;
+        console.log("Nose x = " + noseX);
+        console.log("Nose y = " + noseY);
+    }
+}
+
+function draw()
+{
+    image(video,0,0,300,300);
+    image(lipstick,noseX,noseY,43,30);
+}
+
+function takeSnapshot()
+{
+    save("pic.png");
+}
